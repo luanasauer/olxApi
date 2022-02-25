1b5b57ca378e390115e98e383860184b95852a28
Luana Sauer <luanasauer23@gmail.com>
2022-02-25 11:57:41 -0300

    feat: criando validators

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,19 @@
+const { validationResult } = require("express-validator");
+
+module.exports = {
+
+    signin: async (req, res) =>{
+
+    },
+    signup: async (req, res) =>{
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.json({error: errors.mapped()});
+            return;
+        }
+
+        res.json({tudocerto: true});
+
+    },
+};
